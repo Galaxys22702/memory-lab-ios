e96769260ff751cad5f34ffa30369f8fcf53bdb0
Galaxys22702 <109320424+Galaxys22702@users.noreply.github.com>
2026-09-20 21:33:13 -0700

    Use Mach task info for iOS memory footprint

--- MemoryLab/MemoryProbe.c
+++ MemoryLab/MemoryProbe.c
@@ -1,23 +1,46 @@
 #include "MemoryProbe.h"
 #include <os/proc.h>
-#include <libproc.h>
-#include <sys/resource.h>
-#include <unistd.h>
+#include <mach/mach.h>
 
 uint64_t ml_available_memory(void) {
     return (uint64_t)os_proc_available_memory();
 }
 
-static int ml_rusage(rusage_info_current *info) {
-    return proc_pid_rusage(getpid(), RUSAGE_INFO_CURRENT, (rusage_info_t *)info);
+static int ml_task_vm_info(task_vm_info_data_t *info, mach_msg_type_number_t *count) {
+    *count = TASK_VM_INFO_COUNT;
+    kern_return_t kr = task_info(
+        mach_task_self(),
+        TASK_VM_INFO,
+        (task_info_t)info,
+        count
+    );
+    return kr == KERN_SUCCESS ? 0 : -1;
 }
 
 uint64_t ml_phys_footprint(void) {
-    rusage_info_current info = {0};
-    return ml_rusage(&info) == 0 ? info.ri_phys_footprint : 0;
+    task_vm_info_data_t info = {0};
+    mach_msg_type_number_t count = 0;
+
+    if (ml_task_vm_info(&info, &count) != 0) {
+        return 0;
+    }
+
+    return (uint64_t)info.phys_footprint;
 }
 
 uint64_t ml_peak_footprint(void) {
-    rusage_info_current info = {0};
-    return ml_rusage(&info) == 0 ? info.ri_lifetime_max_phys_footprint : 0;
+    task_vm_info_data_t info = {0};
+    mach_msg_type_number_t count = 0;
+
+    if (ml_task_vm_info(&info, &count) != 0) {
+        return 0;
+    }
+
+#if defined(TASK_VM_INFO_REV3_COUNT)
+    if (count >= TASK_VM_INFO_REV3_COUNT && info.ledger_phys_footprint_peak > 0) {
+        return (uint64_t)info.ledger_phys_footprint_peak;
+    }
+#endif
+
+    return (uint64_t)info.phys_footprint;
 }
